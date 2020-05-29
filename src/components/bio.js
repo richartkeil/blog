70cc0dffb6a1bcb6f494ab235f639370f5bf5614
Richard Keil <richartkeil@gmail.com>
2020-05-29 14:47:32 -0400

    Adjust bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -38,8 +38,8 @@ const Bio = () => {
       />
       <div>
         I'm Richard and this is my blog. I try to craft articles like little
-        tasty pieces of knowledge - easy to understand so I can get better at
-        breaking down things 💡 (<a href="https://github.com/richartkeil">Github</a>, <a href="https://www.instagram.com/richartkeil/">Instagram</a> and <a href="https://twitter.com/richartkeil">Twitter</a>)
+        tasty pieces of knowledge — easy to understand so I can get better at
+        breaking down things. (<a href="https://github.com/richartkeil">Github</a>, <a href="https://www.instagram.com/richartkeil/">Instagram</a> and <a href="https://twitter.com/richartkeil">Twitter</a>)
       </div>
     </div>
   )
