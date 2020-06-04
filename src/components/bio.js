5302635ef026d8d25366ca55d08328257835a60f
Richard Keil <richartkeil@gmail.com>
2020-06-04 09:46:05 -0400

    Make Bio responsive

--- src/components/bio.js
+++ src/components/bio.js
@@ -1,8 +1,28 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Image from "gatsby-image"
+import styled from "styled-components"
 
 import { rhythm } from "../utils/typography"
+
+const BioContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  @media (max-width: 450px) {
+    flex-direction: column;
+    align-items: center;
+  }
+`
+
+const ProfileImage = styled(Image)`
+  margin-right: ${rhythm(1 / 2)};
+  margin-bottom: 0;
+  min-width: 80px;
+  border-radius: 100%;
+  @media (max-width: 450px) {
+    margin-bottom: ${rhythm(1 / 2)};
+  }
+`
 
 const Bio = () => {
   const data = useStaticQuery(graphql`
@@ -18,20 +38,10 @@ const Bio = () => {
   `)
 
   return (
-    <div
-      style={{
-        display: `flex`,
-      }}
-    >
-      <Image
+    <BioContainer>
+      <ProfileImage
         fixed={data.avatar.childImageSharp.fixed}
         alt="Richard Keil"
-        style={{
-          marginRight: rhythm(1 / 2),
-          marginBottom: 0,
-          minWidth: 80,
-          borderRadius: `100%`,
-        }}
         imgStyle={{
           borderRadius: `50%`,
         }}
@@ -41,11 +51,11 @@ const Bio = () => {
         to understand things and become better at explaining them.
         <div>
           <a href="https://github.com/richartkeil">Github</a> •{" "}
-          <a href="https://www.instagram.com/richartkeil/">Instagram</a> •{" "}
+          <a href="https://www.instagram.com/richartkeil/">Art on Instagram</a> •{" "}
           <a href="https://twitter.com/richartkeil">Twitter</a>
         </div>
       </div>
-    </div>
+    </BioContainer>
   )
 }
 
